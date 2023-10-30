6714c3af7a9adc5142f74251528fb66340950aee
ZamoraJulipan <134815443+ZamoraJulipan@users.noreply.github.com>
2023-10-30 18:19:47 -0500

    Initial

---
+++ main.py
@@ -0,0 +1,33 @@
+from plyer import notification
+from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
+
+KV = '''
+<ui>:
+    MDScreen:
+        MDBoxLayout:
+            MDRectangleFlatButton:
+                text: 'notificar'
+                on_release:
+                    app.notificar()
+'''
+
+class Ui(ScreenManager):
+    pass
+
+class dev(MDApp):
+    def build(self):
+        Builder.load_string(KV)
+        return Ui()
+    
+    def notificar(self):
+        notification.notify(
+            title = 'Batipython',  
+            message = 'Esta es una notificacion',  
+            app_icon = None,  
+            timeout = 10,  
+            toast = True
+        )
+
+dev().run()
