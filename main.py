ed4a57950d79c718011ea96cfded47f491067477
ZamoraJulipan <134815443+ZamoraJulipan@users.noreply.github.com>
2023-11-02 12:43:07 -0500

    Initial

---
+++ main.py
@@ -0,0 +1,21 @@
+from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
+
+class Ui(ScreenManager):
+    pass
+
+class main(MDApp):
+    def build(self):
+        Builder.load_file('main.kv')
+        return Ui()
+    
+    def guardar(self,texto):
+        try:
+            with open('.\\sdcard\\Documents\\texto_nuevo','w+') as archivo:
+                archivo.write(texto)
+                archivo.close()
+        except TypeError:
+            pass
+
+main().run()
