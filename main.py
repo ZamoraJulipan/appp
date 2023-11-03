185d5ce1f1df4b0cc112500ae62f67362f04ecd8
ZamoraJulipan <134815443+ZamoraJulipan@users.noreply.github.com>
2023-11-03 11:11:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,14 @@
 from kivymd.app import MDApp
-from kivy.uix.screenmanager import ScreenManager
-from kivy.lang import Builder
+from kivy.uix.widget import Widget
 from kivymd.toast import toast
-import os
 
-class Ui(ScreenManager):
-    pass
+class TouchDetector(Widget):
+    def on_touch_down(self, touch):
+        # Se llama cuando se toca la pantalla
+        toast("Se ha tocado la pantalla")
 
-class main(MDApp):
+class MyApp(MDApp):
     def build(self):
-        Builder.load_file('main.kv')
-        return Ui()
-    
-    def comprobar(self):
-        if os.path.exists('texto_nuevo.txt'):
-            toast('Sí')
-        else:
-            toast('No')
+        return TouchDetector()
 
-    def guardar(self,texto):
-        try:
-            with open('texto_nuevo.txt','w+') as archivo:
-                archivo.write(texto)
-                archivo.close()
-        except TypeError:
-            pass
-
-main().run()
+MyApp().run()
