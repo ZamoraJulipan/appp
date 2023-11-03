ecc9a4c2ac24a22c9c86983a24dda9bbf82003ba
ZamoraJulipan <134815443+ZamoraJulipan@users.noreply.github.com>
2023-11-02 19:36:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager
 from kivy.lang import Builder
+from kivymd.toast import toast
+import os
 
 class Ui(ScreenManager):
     pass
@@ -10,9 +12,15 @@ class main(MDApp):
         Builder.load_file('main.kv')
         return Ui()
     
+    def comprobar(self):
+        if os.path.exists('texto_nuevo.txt'):
+            toast('Sí')
+        else:
+            toast('No')
+
     def guardar(self,texto):
         try:
-            with open('\\sdcard\\texto_nuevo.txt','w+') as archivo:
+            with open('texto_nuevo.txt','w+') as archivo:
                 archivo.write(texto)
                 archivo.close()
         except TypeError:
